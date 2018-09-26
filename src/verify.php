a06584c1b0e7fce218ccfa3876003aecba9a6184
Crypto Jojo <cryptojojo1@gmail.com>
2018-09-26 18:04:36 -0500

    Login working

--- src/verify.php
+++ src/verify.php
@@ -1,7 +1,19 @@
+
 <html>
-    <body>
 
-        <?php header('Location: home.html'); ?>
 
-    </body>
-</html> 
+
+    <?php
+    $Query_username = 'admin';
+
+    $Query_pass = '12345';
+
+
+    if (($_POST["name"] == $Query_username) && $_POST["pass"] == $Query_pass) {
+        header('Location: home.html');
+    } else {
+        header('Location: home.html');
+    }
+    ?>
+
+</html>
