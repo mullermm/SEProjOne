92426bad736afdf3b6ae0a65324274060106d88b
Maxwell Herron <maxwellherron5@gmail.com>
2018-09-26 20:17:49 -0500

    file restructure

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Login from "./Components/Login";
 
 class App extends Component{
     render() {
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import "../index.css";
 
 ReactDOM.render(
     <Router>
