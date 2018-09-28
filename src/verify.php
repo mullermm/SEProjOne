ecb2520eb447deffa8cd8ffa6724835811ee55be
Crypto Jojo <cryptojojo1@gmail.com>
2018-09-27 19:09:54 -0500

    Added student home, verify interprets student vs admin and has incorrect page

--- src/verify.php
+++ src/verify.php
@@ -9,10 +9,12 @@
     $Query_pass = '12345';
 
 
-    if (($_POST["name"] == $Query_username) && $_POST["pass"] == $Query_pass) {
-        header('Location: home.html');
-    } else {
-        header('Location: sdfsfsd.html');
+    if (($_POST["name"] == 'admin') && ($_POST["pass"] == '12345')) {
+        header('Location: home-admin.html');
+    } elseif (($_POST["name"] == 'student') && ($_POST["pass"] == '12345')) {
+        header('Location: home-student.html');
+    } else{
+        header('Location: incorrect.html');
     }
     ?>
 
