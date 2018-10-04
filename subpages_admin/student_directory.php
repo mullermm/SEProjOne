2549838882909bf201817b526a89ce7269f17c24
Crypto Jojo <cryptojojo1@gmail.com>
2018-10-04 17:50:57 -0500

    Organized css files into their own folder

--- subpages_admin/student_directory.php
+++ subpages_admin/student_directory.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link rel="stylesheet" href="../src/style-home.css" type="text/css" />
+        <link rel="stylesheet" href="../css/style-home.css" type="text/css" />
         <link href="https://unpkg.com/ionicons@4.2.0/dist/css/ionicons.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
 
