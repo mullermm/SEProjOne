4241d38a14012fbc37da2a043cef42c739824765
Maxwell Herron <maxwellherron5@gmail.com>
2018-10-04 23:42:38 -0500

    Added the java portion

---
+++ JAVA Scanner/src/Course.java
@@ -0,0 +1,91 @@
+/**
+ * @author Maxwell Herron
+ * This class embodies the course object, which will hold all the information of each course
+ */
+public class Course
+{
+    String name;
+    String description;
+    double creditHours;
+    int courseLevel;
+    String department;
+    String prerequisites;
+
+    /**
+     * This is the constructor method for the Course object
+     * @param inName
+     * @param inCreditHours
+     * @param inCourseLevel
+     * @param inDepartment
+     * @param inPrerequisites
+     */
+    public Course(String inName, double inCreditHours, String inDescription, int inCourseLevel, String inDepartment, String inPrerequisites)
+    {
+        this.name = inName;
+        this.creditHours = inCreditHours;
+        this.description = inDescription;
+        this.courseLevel = inCourseLevel;
+        this.department = inDepartment;
+        this.prerequisites = inPrerequisites;
+    }
+
+    public Course() { }
+
+/********************************* GETTERS AND SETTERS **************************************************/
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public int getCourseLevel() {
+        return courseLevel;
+    }
+
+    public void setCourseLevel(int courseLevel) {
+        this.courseLevel = courseLevel;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+/*******************************************************************************************************/
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", creditHours=" + creditHours +
+                ", courseLevel=" + courseLevel +
+                ", department='" + department + '\'' +
+                ", prerequisites='" + prerequisites + '\'' +
+                '}';
+    }
+}
---
+++ JAVA Scanner/src/TextScanner.java
@@ -0,0 +1,57 @@
+/**
+ * This class will create the TextScanner object, which is responsible for parsing through courses.txt
+ * and then creating a course object out of each course. It then pushes those into an arraylist and writes
+ * them into the courses.csv file
+ */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TextScanner
+{
+    /**
+     * This is the generic constructor for the TextScanner object
+     */
+    public TextScanner() { }
+
+    /**
+     * This method will parse through courses.txt and create a course object out of each course
+     * and then push each course object into the arraylist courseList
+     * @param inFile the file being read from
+     * @return courseList, the arraylist holding all the course objects
+     */
+    public ArrayList<Course> scan(File inFile)
+    {
+        // This ArrayList will hold all of the course objects to later be printed out line by line in courses.csv
+        ArrayList courseList = new ArrayList();
+
+        // Creates the file reader and writer and handles any exceptions that this may cause
+        try{
+            Scanner reader = new Scanner(inFile);
+
+            while(reader.hasNextLine())
+            {
+                Course course = new Course();
+
+                courseList.add(course);
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        return courseList;
+    }
+
+    /**
+     * This method will iterate through each course object in inList and write their toString() call into
+     * courses.csv
+     * @param inFile the file being written to
+     * @param inList the arraylist being iterated through
+     */
+    public void writeToFile(File inFile, ArrayList inList)
+    {
+        ArrayList<Course> courseList = new ArrayList();
+        courseList = inList;
+    }
+}
