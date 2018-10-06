b80e5c8e67a691922989b9a15c5588cf30a5c95c
Maxwell Herron <maxwellherron5@gmail.com>
2018-10-05 22:32:36 -0500

    Added test unit for verify.php

---
+++ src/Test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxwellherron5
+ * Date: 10/5/18
+ * Time: 10:30 PM
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testName()
+    {
+        // sanitize input to avois injections
+        $login_query = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+        $password_query = filter_var($_POST["pass"], FILTER_SANITIZE_STRING);
+
+
+
+        // variable initialization
+        $current_user = '';
+        $the_big_array = [];
+        $filename = 'Augs_Users.csv';
+        $failure = TRUE;
+
+        // putting the database (csv) into a multidimensional array - super unsecure
+        if (($h = fopen("{$filename}", "r")) !== FALSE) {
+            while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+                $data = str_replace(' ', '', $data);
+                $the_big_array[] = $data;
+            }
+
+            fclose($h);
+        }
+
+
+        for ($i = 0; $i < sizeof($the_big_array); $i++) {
+
+            if ($the_big_array[$i][2] == $login_query) {
+
+                if ($the_big_array[$i][3] == $password_query) {
+
+                    $current_user = (string) $the_big_array[$i][0] . " " . (string) $the_big_array[$i][1];
+
+                    session_start();
+                    $_SESSION['current_user'] = $current_user;
+
+                    $failure = FALSE;
+
+                    if ($the_big_array[$i][4] == "student") {
+                        header('Location: home-student.php');
+                    } elseif ($the_big_array[$i][4] == "admin") {
+                        header('Location: home-admin.php');
+                    }
+                }
+            }
+        }
+
+        if ($failure) {
+            session_start();
+            $_SESSION['login_fail'] = True;
+            header('Location: ../public/index.php');
+
+        }
+    }
+
+}
