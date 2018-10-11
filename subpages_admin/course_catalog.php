6442870e045b4d91f189d54472ab3e5df67b2e53
Maxwell Herron <maxwellherron5@gmail.com>
2018-10-10 21:05:14 -0500

    fixed the csv

--- subpages_admin/course_catalog.php
+++ subpages_admin/course_catalog.php
@@ -26,7 +26,7 @@
                     <a href="../src/home-student.php"><img src="../src/images/logo.png" class="img-fluid nav-logo-desktop" alt="Company Logo"></a>
                     <ul class="navbar-nav ml-auto nav-right" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
                         <li class="nav-item nav-custom-link">
-                            <a class="nav-link" href="../src/home-student.php">Return to Portal Home <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+                            <a class="nav-link" href="../src/home-admin.php">Return to Portal Home <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
                         </li>
                         <li class="nav-item nav-custom-link">
 
