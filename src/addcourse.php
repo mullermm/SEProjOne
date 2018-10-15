3cdf0d09d85f357051104238018a231cde197524
Maxwell Herron <maxwellherron5@gmail.com>
2018-10-15 03:05:17 -0500

    got the add user and add course functionalities to work

--- src/addcourse.php
+++ src/addcourse.php
@@ -1,6 +1,8 @@
 <html>
 
     <?php
+
+
     $department_in = filter_var($_POST["department"], FILTER_SANITIZE_STRING);
     $course_number_in = filter_var($_POST["course-number"], FILTER_SANITIZE_STRING);
     $course_title_in = filter_var($_POST["course-title"], FILTER_SANITIZE_STRING);
@@ -9,16 +11,20 @@
     $core_curriculum_in = filter_var($_POST["core-curriculum"], FILTER_SANITIZE_STRING);
     $prerequisites_in = filter_var($_POST["prerequisites"], FILTER_SANITIZE_STRING);
 
-    $csv = fopen("courses.csv", "a");
-
-    $entry = "\n" . $department_in . '~' . $course_number_in . '~' . $course_title_in . '~' . $credits_in . '~' . $course_description_in . '~' . $core_curriculum_in . '~' . $prerequisites_in;
+    $file = "courses.csv";
 
 
-    fputs($csv, $entry);
+    $csv = fopen($file, "a") or die("Unable to open file!");
+
+    $entry = array($department_in . '~' . $course_number_in . '~' . $course_title_in . '~' . $credits_in . '~' . $course_description_in . '~' . $core_curriculum_in . '~' . $prerequisites_in);
+
+    fputcsv($csv, $entry);
 
     fclose($csv);
 
     header('Location: ../subpages_admin/course_catalog.php');
+
+
     ?>
 
 </html>
