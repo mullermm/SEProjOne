24afe9a4d767ab749540ddaa66be00b88c63243e
Matthew Muller <mmuller914@gmail.com>
2018-11-21 16:17:28 -0600

    OMG I GOT THE LIST TO LOAD :D

--- Utilities/PrereqScanner/src/PrereqListBuilder.java
+++ Utilities/PrereqScanner/src/PrereqListBuilder.java
@@ -1,9 +1,6 @@
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -32,12 +29,12 @@ public class PrereqListBuilder {
             while (scan.hasNext()) {
 
                 scan.useDelimiter("~");                         //delimeter sperating the name from prereqs
-                br.write("['");                             //"[" for the course and the " ' " for the course name string
+                br.write("[");                             //"[" for the course and the " ' " for the course name string
 
                 /*The line below add the name and the closing " ' ". The reason we have a .replace
                 * is because the /n character at the end of each line in filterdCSV is not
                 * dealt with by the scanner. This replace deals with it*/
-                br.write(scan.next().replace("\n","") + "',");
+                br.write(scan.next().replace("\n","") + ",");
                 scan.useDelimiter("\n|;");                       //delimeter seperating the end of the prereqs or seperate prereqs
                 input = scan.next();                             //Store the next token
                 doneWithLine = false;
@@ -46,7 +43,7 @@ public class PrereqListBuilder {
                     input = input.replace("~", "");  //Remove the delimiter between name and prereqs
                     /*If there is no prereqs CASE1 BREAK*/
                     if(input.equals("NONE%")){
-                        br.write("['NONE']]");                      //append no prereqs
+                        br.write("NONE]");                      //append no prereqs
                         doneWithLine = true;
                         break;
                     }
@@ -70,13 +67,13 @@ public class PrereqListBuilder {
                     else{
                         input = input.replace("%", "");
                         String[] classes = input.split(",|\\*");            //split with the * too because classes being conccurrent
-                        br.write("['");
+                        br.write("[");
                         for(int i = 0; i < classes.length; i++){                  //for all the classes required
                             if(i + 1 != classes.length){                          //if its not the last class in the list
-                                br.write(classes[i] + "','");                 //add the classes
+                                br.write(classes[i] + "|");                  //add the classes
                             }
                             else{                                                 //its the last class
-                                br.write(classes[i] + "']]");                 //write the class and close the list
+                                br.write(classes[i] + "]]");                 //write the class and close the list
                                 doneWithLine = true;
                                 break;
                             }
@@ -91,11 +88,40 @@ public class PrereqListBuilder {
             br.write("]");     //close the list of lists
             br.close();
 
+            File filered = new File("prereqList.txt");
+
+            File prereqScannerFile = new File("../PreReq/prereqList.txt");
+            CopyFile(filered, prereqScannerFile);
+
         }
         catch(IOException e){
             System.out.println(e.getStackTrace());
         }
 
+    }
+
+    /**
+     *This method takes two files and copies ton contents of the inFile to the outFile.
+     *
+     * @author https://www.tutorialspoint.com/javaexamples/file_copy.htm
+     * */
+    public static void CopyFile(File inFile, File outFile) {
+        FileInputStream ins = null;
+        FileOutputStream outs = null;
+        try {
+            ins = new FileInputStream(inFile);
+            outs = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = ins.read(buffer)) > 0) {
+                outs.write(buffer, 0, length);
+            }
+            ins.close();
+            outs.close();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
     /**
@@ -108,38 +134,20 @@ public class PrereqListBuilder {
         try {
             br.write("[");
             String[] prereq = input.split("\\@");      //split the string into an array into the number and the classes
-            br.write(prereq[0] + ",");                //Write the number of needed classes completed
+            br.write(prereq[0] + "|");                //Write the number of needed classes completed
             String[] classes = prereq[1].split(",");  //split the classes into an array
 
             for (int i = 0; i < classes.length; i++) {
                 if (i + 1 != classes.length) {
-                    br.write("'" + classes[i] + "',");
+                    br.write("" + classes[i] + "|");
                 } else {
-                    br.write("'" + classes[i] + "']");
+                    br.write("" + classes[i] + "]");
                 }
             }
         }
         catch(IOException e){
             System.out.println("Cant write to prereqList in method variablePrereq");
         }
-    }
-
-    /**
-     * This method will check to see if a string is an integer. If so, it returns true, else it returns false
-     *
-     * @author Oscar Lopez - https://stackoverflow.com/questions/8336607/how-to-check-if-the-value-is-integer-in-java
-     */
-    public static boolean isInteger(String s) {
-        boolean isValidInteger = false;
-        try {
-            Integer.parseInt(s);
-            isValidInteger = true;
-        }
-        catch (NumberFormatException ex)
-        {
-            //Is not an integer
-        }
-        return isValidInteger;
     }
 
 }
