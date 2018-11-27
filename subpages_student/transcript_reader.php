313c95c1839f835768d57826d03e32281d08b871
Matthew Muller <mmuller914@gmail.com>
2018-11-27 16:28:30 -0600

    Got the transcript upload to save

--- subpages_student/transcript_reader.php
+++ subpages_student/transcript_reader.php
@@ -1,14 +1,12 @@
 <?php
-  $targetdir = '../subpages_student';   
+  $targetdir = '../';   
       // name of the directory where the files should be stored
-  $targetfile = $targetdir.$_FILES['file']['name'];
+      
 
-  if (move_uploaded_file($_FILES['file']['tmp_name'], $targetfile)) {
-    header("Location: ../subpages_student/course_planner.php");
-  } else { 
-    header("Location: ../subpages_student/course_catalog.php");
-  }
+  $targetfile = "Transcript.txt";
 
-
+  move_uploaded_file($_FILES['file']['tmp_name'], $targetdir . $targetfile);
+  
   header("Location: ../subpages_student/course_planner.php");
+  
 ?>
