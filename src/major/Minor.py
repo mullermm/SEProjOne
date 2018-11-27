231f2303c0cc41b4d1be2b7b6827f5dc50ed71ee
Jackson Nelson <nelsonj6@augsburg.edu>
2018-11-27 15:29:43 -0600

    More minors

--- src/major/Minor.py
+++ src/major/Minor.py
@@ -1,9 +1,109 @@
 ##Minor.py
 
+def accounting():
+	section1 = ['ACC221', 'ACC222', 'ACC322', 'ACC323', 'ACC324']
+	# 1 of
+	section2 = ['ECO112', 'ECO113', 'FIN331', 'MIS260', 'MIS379']
+	
+def business_administration():
+	section1 = ['ACC221', 'ACC222', 'BUS242', 'MKT252']
+	# 1 of
+	section2 = ['MIS264', 'MIS379', 'FIN331']
+	# 1 of
+	section3 = ['ECO112', 'ECO113']
+	
+def business_analytics():
+	section1 = ['CSC170', 'CSC310', 'MIS375', 'MIS479']
+	# 1 of
+	section2 = ['MAT163', 'MAT164', 'MIS379']
+	# 1 of
+	section3 = ['MAT171', 'MAT145', 'MAT122']
+	# 1 of
+	section4 = ['MIS270', 'CSC352']
 
+def chemistry():
+	section1 = ['CHM115', 'CHM115L', 'CHM116', 'CHM116L', 'CHM280', 'CHM280L']
+	# 2 of
+	section2 = ['CHM251', 'CHM251L', 'CHM252', 'CHM252L', 'CHM362', 'CHM368', 'CHM370', 'CHM464', 'CHM481', 'CHM482', 'CHM495', 'CHM499']
 
+def film():
+	section1 = ['COM120', 'COM247', 'ENL241', 'ENL371', 'FLM180', 'FLM420']
+
+def communication_studies():
+	section1 = ['COM111', 'COM280', 'COM329', 'COM351']
+	#2 of
+	section2 = ['COM120', 'COM243', 'COM247', 'COM254', 'COM260', 'COM281', 'COM299', 'COM321', 'COM345', 'COM345', 'COM355', 'COM399', 'COM405', 'COM415', 'COM480', 'COM490', 'COM495', 'COM499']
+
+def computer_science():
+	section1 = ['CSC160', 'CSC170', 'CSC210']
+	# 1 of
+	section2 = ['CSC320', 'CSC345']
+	# 1 of
+	section3 = ['MAT171', 'MAT271']
+	# 1 of
+	section4 = ['CSC310', 'CSC320', 'CSC345', 'CSC352', 'CSC353', 'CSC373', 'CSC385', 'CSC395', 'CSC431', 'CSC450', 'CSC451', 'CSC457', 'CSC495', 'CSC499']
+
+def culture_and_social_justice():
+	# 1 of
+	section1 = ['AIS105', 'ANT141', 'CCS100']
+	# 1 of
+	section2 = ['AIS208', 'AIS233', 'AIS364', 'COM329', 'PSY261', 'SOC240', 'SOC265', 'SOC290', 'THR245', 'WST305', 'WST315', 'WST324']
+	# 1 of
+	section3 = ['AIS105', 'AIS205', 'AIS332', 'ENL355', 'POL122']
+	# 1 of
+	section4 = ['AIS105', 'AIS205', 'AIS264', 'AIS320', 'AIS332', 'ART290', 'ART390', 'ENL250', 'ENL251', 'ENL255', 'ENL350', 'ENL365', 'HIS236', 'HIS241', 'HIS242', 'HIS243', 'THR270']
+	# 1 of
+	section5 = ['INS495', 'AIS405']
+
+def economics():
+	section1 = ['ECO112', 'ECO113', 'ECO312', 'ECO313']
+	## 1 of
+	section2 = ['ECO315', 'ECO318', 'ECO350', 'ECO360', 'ECO365', 'ECO370', 'ECO399', 'ECO416', 'ECO490', 'ECO495', 'ECO499']
+
+def creative_writing():
+	section1 = ['ENL226']
+	## 2 of
+	section2 = ['ENL320', 'ENL321', 'ENL322', 'ENL323']
+	# 1 of
+	section3 = ['ENL320', 'ENL322', 'ENL324', 'ENL325', 'ENL329', 'ENL420']
+	# 1 of
+	section4 = ['ENL220', 'ENL221', 'ENL223', 'ENL227', 'ENL228', 'ENL229', 'ENL320', 'ENL321', 'ENL322', 'ENL323', 'ENL324', 'ENL324', 'ENL326', 'ENL327', 'ENL329', 'ENL420']
+
+def literature():
+	## 1 of
+	section1 = ['ENL220', 'ENL221']
+	# 4 of
+	section2 = ['ENL220', 'ENL221', 'ENL223', 'ENL227', 'ENL228', 'ENL229', 'ENL320', 'ENL321', 'ENL322', 'ENL323', 'ENL324', 'ENL324', 'ENL326', 'ENL327', 'ENL329', 'ENL420']
+	
+def english():
+	# 5 of
+	section1 = ['ENL220', 'ENL221', 'ENL220', 'ENL221', 'ENL223', 'ENL227', 'ENL228', 'ENL229', 'ENL320', 'ENL321', 'ENL322', 'ENL323', 'ENL324', 'ENL324', 'ENL326', 'ENL327', 'ENL329', 'ENL420']
+
+def environmental_studies():
+	section1 = ['ENV100', 'ENV120']
+	# 3 of
+	section2 = ['COM260', 'ENL270', 'HIS316', 'RLN333', 'WST313', 'ECO365', 'ENV310', 'POL241', 'POL325', 'POL341', 'WST341', 'SOC380', 'SWK210', 'BIO152', 'BIO481', 'CHM115', 'CHM115L']
+
+def womens_studies():
+	## All
+	section1 = ['WST201', 'WST315']
+	# 3 total, 1 upper
+	section2 = ['WST199', 'WST220', 'WST250', 'WST281', 'WST299', 'WST305', 'WST313', 'WST324', 'WST335', 'WST341', 'WST352', 'WST353', 'WST355', 'WST357', 'WST362', 'WST366', 'WST396', 'WST481', 'WST485', 'WST490']
+
+def global_womens_and_gender_studies():
+	section1 = ['POL359', 'WST201', 'WST315']
+	## 2 of, 1 upper
+	section2 = ['AIS208', 'AIS408', 'AIS233', 'AIS305', 'AIS320', 'ENL365', 'HIS195', 'WST250', 'WST281', 'WST481', 'WST313', 'WST341', 'WST357', 'WST362', 'WST366']
+	
+def sexuality_studies():
+	section1 = ['SOC266', 'WST201', 'WST305']
+	## 2 of, 1 upper
+	section2 = ['HPE316', 'WST250', 'WST315', 'WST281', 'WST481']
+	
 def exercise_science():
+	## All
 	section1 = ['BIO103', 'HPE104', 'HPE350', 'HPE350L', 'HPE351', 'HPE351L']
+	## 8 credits of
 	section2 = ['HPE114', 'HPE215', 'HPE220', 'HPE280', 'HPE304', 'HPE315', 'HPE357']
 
 def health_education():
@@ -11,6 +111,24 @@ def health_education():
 	
 def physical_education():
 	section1 = ['HPE104','HPE115', 'HPE205', 'HPE324', 'HPE334', 'HPE351']
+	
+def history():
+	## 5 total, 3 upper
+	section1 = ['HIS101', 'HIS102', 'HIS103', 'HIS104', 'HIS120', 'HIS121', 'HIS122', 'HIS140', 'HIS150', 'HIS155', 'HIS162', 'HIS170', 'HIS195', 'HIS199', 'HIS225', 'HIS234', 'HIS236', 'HIS241', 'HIS242', 'HIS243', 'HIS249', 'HIS280', 'HIS282', 'HIS299', 'HIS300', 'HIS311', 'HIS316', 'HIS323', 'HIS327', 'HIS331', 'HIS332', 'HIS335', 'HIS336', 'HIS338', 'HIS343', 'HIS346', 'HIS348', 'HIS349', 'HIS350', 'HIS352', 'HIS354', 'HIS355', 'HIS357', 'HIS360', 'HIS361', 'HIS369', 'HIS370', 'HIS374', 'HIS378', 'HIS397', 'HIS440', 'HIS462', 'HIS468', 'HIS474', 'HIS480', 'HIS497', 'HIS498']
+	
+
+def french():
+	## Four of
+	section1 = ['FRE212', 'FRE295', 'FRE299', 'FRE311', 'FRE331', 'FRE332', 'FRE351', 'FRE353', 'FRE399', 'FRE411', 'FRE495', 'FRE499']
+	
+def german():
+	## Four of
+	section1 = ['GER212', 'GER295', 'GER299', 'GER311', 'GER331', 'GER332', 'GER351', 'GER354', 'GER399', 'GER411', 'GER495', 'GER499']
+	
+def spanish():
+	## Four of
+	section1 = ['SPA212', 'SPA218', 'SPA220', 'SPA224', 'SPA225', 'SPA231', 'SPA248', 'SPA295', 'SPA299', 'SPA311', 'SPA312', 'SPA316', 'SPA325', 'SPA331', 'SPA332', 'SPA334', 'SPA335', 'SPA336', 'SPA337', 'SPA352', 'SPA353', 'SPA354', 'SPA356', 'SPA357', 'SPA397', 'SPA411', 'SPA495', 'SPA499']
+	
 	
 def international_relations():
 	section1 = ['ECO112', 'ECO113']
@@ -24,100 +142,141 @@ def peace_and_global_studies():
 	section2 = ['ECO365', 'ECO495', 'ENL365', 'HIS332', 'HIS346', 'HIS350', 'INS312', 'POL341', 'WST341', 'POL353', 'RLN333', 'RLN336', 'RLN376', 'SOC240']
 	
 def leadership_studies():
+	## Both
 	section1 = ['LST205', 'LST485']
+	## 3 total, at least 1 from both section2 and section3
 	section2 = ['COM321', 'COM345', 'COM355', 'COM480', 'ENL220', 'ENL385']
 	section3 = ['COM329', 'INS359', 'SOC265', 'SOC349', 'SWK280']
 
 def mathematics():
+	## Both
 	section1 = ['MAT145', 'MAT146']
+	## 3 total, 1 above 300
 	section2 = ['MAT213', 'MAT245', 'MAT246', 'MAT252', 'MAT271', 'MAT273', 'MAT287', 'MAT304', 'MAT314', 'MAT324', 'MAT355', 'MAT363', 'MAT369', 'MAT373', 'MAT374', 'MAT377', 'MAT394', 'MAT395', 'MAT399', 'MAT499', 'ECO416', 'PHY327']
 
 def statistics():
+	## 1 of
 	section1 = ['MAT163', 'MAT164', 'MIS379', 'PSY215', 'SOC362']
+	## Both
 	section2 = ['MAT213', 'MAT273']
+	## 1 of
 	section3 = ['BIO481', 'ECO490', 'MAT374', 'MKT352', 'POL483', 'PSY315', 'SOC363']
+	## 2 of
 	section4 = ['MAT373', 'MAT394', 'CSC170', 'CSC310', 'CSC352', 'MIS270', 'MIS479', 'PSY491']
 	
 def medieval_studies():
 	section1 = ['HUM120']
+	## 1 of
 	section2 = ['ENL330', 'ENL332', 'ENL360', 'ENL361', 'SPA352']
+	## 1 of
 	section3 = ['HIS378', 'PHI242', 'REL362']
+	## 1 of
 	section4 = ['ART386', 'ART387', 'MUS231', 'THR361']
+	## 1 of
 	section5 = ['HIS369', 'HIS370', 'HIS374', 'HIS440', 'POL380', 'SPA331']
 	
 def music():
 	section1 = ['MUS113', 'MUS114', 'MUS341']
+	## 1 of
 	section2 = ['MUS231', 'MUS232']
+	## 1 of
 	section3 = ['MUS130', 'MUS220', 'MUS231', 'MUS232', 'MUS241', 'MUS331', 'MUS332', 'MUS333', 'MUS334', 'MUS342', 'MUS344']
+	## 4 of
 	section4 = ['MUE111', 'MUE112', 'MUE114', 'MUE115', 'MUE121', 'MUE141']
 	
 def music_business():
 	section1 = ['ACC221', 'ECO113', 'MIS260', 'MUS162', 'MUS205', 'MUS335', 'BUS399', 'MUS399', 'MUS408']
+	## 1 of
 	section2 = ['BUS254', 'BUS301', 'MUS336']
 	
 def new_media():
 	section1 = ['NMS220', 'NMS242', 'ENL242', 'COM415', 'POL371']
+	## 2 of
 	section2 = ['ART102', 'ART124', 'ART125,' 'ART126', 'ART133', 'ART180', 'ART201', 'ART202', 'ART215', 'ART226', 'ART315', 'ART340', 'COM243', 'CSC250', 'CSC431', 'CSC495', 'EDC220', 
 				'ENL221', 'ENL226', 'ENL228', 'ENL229', 'ENL241', 'ENL290', 'ENL324', 'ENL371', 'ENL427', 'FLM180', 'FLM240', 'FLM260', 'FLM495', 'INS330', 'INS331', 'MUS221', 'MUS272', 'MUS336',
 				'NMS230', 'NMS260', 'NMS295', 'NMS320', 'NSM495', 'NMS499', 'PHI260', 'PHY261', 'POL342', 'POL495', 'RLN216']
 				
 def	philosophy():
+	## 2 of
 	section1 = ['PHI241', 'PHI242', 'PHI343', 'PHI344']
+	## 3 of
 	section2 = ['PHI110', 'PHI120', 'PHI125', 'PHI175', 'PHI180', 'PHI230', 'PHI241', 'PHI242', 'PHI260', 'PHI299', 'PHI315', 'PHI343', 'PHI344', 'PHI350', 'PHI370', 'PHI380', 'PHI385', 'PHI410', 'PHI499']
 	
 def physics():
 	section1 = ['PHY121', 'PHY122', 'MAT145', 'MAT146']
+	## 3 of
 	section2 = ['PHY245', 'PHY261', 'PHY299', 'PHY317', 'PHY320', 'PHY327', 'PHY351', 'PHY352', 'PHY362', 'PHY363', 'PHY420', 'PHY430', 'PHY486', 'PHY499']
 	
 def political_science():
 	section1 = ['POL158']
+	## 1 of
 	section2 = ['POL121', 'POL122', 'POL170']
+	## 3 of
 	section3 = ['POL325', 'POL326', 'POL342', 'POL421', 'POL350', 'POL359', 'POL459','POL368', 'POL461', 'POL490', 'POL370', 'POL371', ' POL380', 'POL381', 'POL495']
 
 def political_science_pre_law():
 	section1 = ['POL121', 'POL170']
+	## 2 of
 	section2 = ['COM111', 'ENL220', 'ENL223', 'PHI230']
+	## 2 of
 	section3 = ['POL350', 'POL370', 'POL371', 'POL380']
 
 def political_science_public_policy():
 	section1 = ['POL399']
+	## 1 of
 	section2 = ['POL121', 'POL122']
+	## 1 of
 	section3 = ['POL325', 'POL326']
+	## 3 of
 	section4 = ['POL241', 'POL325', 'POL326', 'POL342', 'POL381', 'POL421', 'POL461', 'ECO312', 'ECO313', 'SOC381']
 
 
 def psychology():
 	section1 = ['PSY105']
+	## 4 of
+	section2 = ['PSY199', 'PSY201', 'PSY203', 'PSY205', 'PSY215', 'PSY235', 'PSY250', 'PSY252', 'PSY253', 'PSY256', 'PSY261', 'PSY262', 'PSY263', 'PSY271', 'PSY282', 'PSY291', 'PSY295', 'PSY299', 'PSY315', 'PSY325', 'PSY354', 'PSY355', 'PSY357', 'PSY359', 'PSY360', 'PSY381', 'PSY385', 'PSY390', 'PSY391', 'PSY396', 'PSY397', 'PSY400', 'PSY410', 'PSY490', 'PSY491', 'PSY493', 'PSY495', 'PSY498']
 	
 def religion():
+	# 4 of
 	section1 = ['RLN209', 'RLN400', 'RLN342', 'RLN370', 'RLN441']
 	
 def sociology():
-	section1 = ['SOC111', 'SOC231', 'SOC240', 'SOC265', 'SOC266', 'SOC277', 'SOC290', 'SOC295', 'SOC299', 'SOC300', 'SOC320', 'SOC349', 'SOC362', 'SOC363', 'SOC375', 'SOC377', 'SOC380', 'SOC381', 'SOC387', 'SOC390', 'SOC399', 'SOC485', 'SOC490', 'SOC495', 'SOC499']
+	section1 = ['SOC121']
+	## 4 of, 2 upper
+	section2 = ['SOC111', 'SOC231', 'SOC240', 'SOC265', 'SOC266', 'SOC277', 'SOC290', 'SOC295', 'SOC299', 'SOC300', 'SOC320', 'SOC349', 'SOC362', 'SOC363', 'SOC375', 'SOC377', 'SOC380', 'SOC381', 'SOC387', 'SOC390', 'SOC399', 'SOC485', 'SOC490', 'SOC495', 'SOC499']
 
 def special_education():
 	section1 = ['EDC410', 'EDU491', 'PSY105', 'SOC231']
+	## 1 of
 	section2 = ['PSY250', 'PSY252', 'PSY253']
+	## 1 of
 	section3 = ['PSY357', 'PSY359', 'SOC265', 'SWK301']
 
 def social_welfare():
 	section1 = ['SWK100', 'SWK301', 'SWK303', 'SWK406', 'SOC265']
+	## 1 of
 	section2 = ['POL121', 'POL158', 'POL325']
 	
 def musical_theater():
+	## 5 of
 	section1 = ['THR232', 'THR235', 'THR237', 'THR255', 'THR365', 'MUE294']
 
 def theater():
+	## 5 of
 	section1 = ['THR232', 'THR235', 'THR237', 'THR255', 'THR365', 'MUE294']
 
 def urban_studies():
 	section1 = ['POL122', 'URB122', 'SOC111', 'URB111', 'SOC381', 'URB381']
+	## 1 of
 	section2 = ['HIS316', 'SOC380', 'URB492', 'ART249', 'ART349', 'HIS249', 'HIS349']
+	## 1 of
 	section1 = ['ART243', 'ART244', 'ART249', 'ART349', 'HIS249', 'HIS349', 'ECO365', 'ENV100', 'HIS316', 'POL241', 'POL325', 'SOC265', 'SOC349', 'SOC380', 'URB199', 'URB295', 'URB299', 'URB395']
 	
 def youth_studies():
 	section1 = ['YST210', 'YST320', 'EDC330', 'EDC331', 'EDC332', 'EDC333']
+	## 1 of
 	section2 = ['PSY203', 'PSY250', 'PSY252', 'SWK303']
+	## 4 of
 	section3 = ['HPE115', 'HPE335', 'POL122', 'URB122', 'POL325', 'PSY262', 'PSY291', 'SOC231', 'SOC265', 'SOC300', 'SOC387', 'WST281', 'WST481']
 	
 	
