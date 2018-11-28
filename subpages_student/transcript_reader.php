464db8894690cf76bf5040d4ca840e594cb2d607
Matthew Muller <mmuller914@gmail.com>
2018-11-27 18:20:03 -0600

    omg i hate git ////

--- subpages_student/transcript_reader.php
+++ subpages_student/transcript_reader.php
@@ -7,10 +7,7 @@
 
   move_uploaded_file($_FILES['file']['tmp_name'], $targetdir . $targetfile);
 
-<<<<<<< HEAD
   $output = shell_exec('python /Applications/XAMPP/xamppfiles/htdocs/TranscriptScanner.py');
-=======
->>>>>>> 12578401c0ece02503569b6a91fb4a363be373d9
 
   header("Location: ../subpages_student/course_planner.php");
 
